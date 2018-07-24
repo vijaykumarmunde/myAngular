d0d3cd78065c0addf9ab52600a47d3ebc7cdadf0
Ash <ashfaq.choudhry@live.com>
2018-07-24 10:41:33 -0500

    Installed bootstrap, jquery, popper.js and font-awesome

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# angular6-quick-start-app
+# Angular 6 quick start app
 Angular quick start sample application with angular-cli 6, bootstrap 4, font-awesome 5
+
+## Installs 
+* bootstrap, jquery and popper:
+1. `npm install bootstrap --save`
+2. `npm install jquery --save`
+3. `npm install popper.js --save`
+
+* font awesome:
+1. `npm install @fortawesome/fontawesome-svg-core --save`
+2. `npm install @fortawesome/free-solid-svg-icons --save`
+3. `npm install @fortawesome/angular-fontawesome --save`
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+library.add(fas);
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
